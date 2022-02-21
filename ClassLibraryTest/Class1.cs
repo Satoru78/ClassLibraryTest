386d551b330e7be0d48e78cc22617257902ce486
Is-lam123 <isl.mirzaev111@gmail.com>
2022-02-21 12:32:07 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryTest/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTest
+{
+    public class Class1
+    {
+        public bool GetPassword (string password)
+        {
+            bool isdigit = false;
+            bool isletter = false;
+            bool isupper = false;
+            bool isspecial = false;
+            try
+            {
+                foreach(var item in password)
+                {
+                    if(char.IsDigit(item))
+                    {
+                        isdigit = true;
+                        break;
+                    }
+                }
+                foreach(var item in password)
+                {
+                    if(char.IsLetter(item))
+                    {
+                        isletter = true;
+                        break;
+                    }
+                }
+                foreach(var item in password)
+                {
+                    if(char.IsUpper(item))
+                    {
+                        isupper = true;
+                        break;
+                    }
+                }
+                foreach(var item in password)
+                {
+                    if (item == '!' || item == '@' || item == '#' || item == '%')
+                    {
+                        isspecial = true;
+                        break;
+                    }
+                }
+
+                return password.Length >= 8 && isdigit && isletter && isupper && isspecial;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool GetEmail(string email)
+        {
+            try
+            {
+                var email_invailed = new MailAddress(email);
+                return email_invailed.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ConsoleUnit-Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTest;
+
+namespace ConsoleUnit_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            Console.Write("Введите пароль:");
+            string password = Console.ReadLine();
+            if (class1.GetPassword(password)) 
+            Console.WriteLine("Корректный пароль");
+            else
+                Console.WriteLine("Некорректный пароль");
+            Console.Write("Введите email:");
+            string email = Console.ReadLine();
+            if (class1.GetEmail(email))
+                Console.WriteLine("Корректный email");
+            else
+                Console.WriteLine("Некорректный email");
+            Console.ReadKey();
+        }
+    }
+}
